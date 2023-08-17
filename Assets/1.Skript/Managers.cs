6969f0f9c2a6ecc41c90321fe94fa0f5dd7114fb
ManoftheStreet <zhijian6527@naver.com>
2023-08-17 21:20:47 +0900

    0817school

---
+++ Assets/1.Skript/Managers.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+
+    static Managers s_instance;
+    public static Managers instance { get { Init();  return s_instance; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+    }
+}
